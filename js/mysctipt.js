720aa3ce467838d27158ea24c7f1f3f294e52a58
Alaikum <carangiadamo@gmail.com>
2022-07-27 15:51:51 +0200

    list made with For

--- js/mysctipt.js
+++ js/mysctipt.js
@@ -1,1 +1,24 @@
-console.log('ciao')
+// Data una lista della spesa, (che vi dovete creare voi)
+// stampare sulla pagina (anche brutalmente, basta che si 
+//     vedano)gli elementi della lista individualmente
+
+console.log('ListaSpesa')
+const spesa = ['carne', 'pasta', 'acqua', 'vino', 'birra', 'pesce',
+    'formaggio', 'patate', 'verdure', 'cartine lunghe']
+
+
+console.log(spesa)
+const ulElement = document.querySelector('.ul__spesa')
+// svuoto ulElement
+ulElement.innerHTML = ''
+console.log(ulElement)
+
+for (let i = 0; i < 10; i++) {
+    const li = `<li class="li__spesa">${spesa[i]} </li>`
+    ulElement.innerHTML += li
+
+}
+
+// for(let i=0; i< spesa.length;i++){
+
+// }
